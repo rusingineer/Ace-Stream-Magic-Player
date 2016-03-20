07ec3b3ba228eb94e12420dcdfe34d9dfaec3e55
Makani <rusingineer@users.noreply.github.com>
2016-03-20 15:08:19 +0300

    Update script_map.js

--- script_map.js
+++ script_map.js
@@ -8,7 +8,7 @@ var magicplayer = {
 magicplayer.script_map = [
 	{
 	    hpattern: '^http(s)?://([^\\.]+\\.)?(new-rutor|rutor)\\(.org|.info|.is)',
-        pattern: '^http(s)?://([^\\.]+\\.)?(new-rutor|rutor)\\.info/torrent/\\d+',
+        pattern: '^http(s)?://([^\\.]+\\.)?(new-rutor|rutor)\\(.org|.info|.is)/torrent/\\d+',
         handler: 'rutor'
     },
     {
